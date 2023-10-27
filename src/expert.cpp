866f604d137735177f8d36505613d952dc8b87ef
ophiase <ophiase@outlook.com>
2023-10-27 20:05:30 +0200

    verify expert algorithm

--- src/expert.cpp
+++ src/expert.cpp
@@ -4,14 +4,19 @@
 /*
     Les taches peuvent être paraléllisées.
     renvois <liste de tache restantes, dans l'ordre, temps d'execution des taches>
+    TODO : CHECK IF IT WORKS
 */
 std::pair<std::vector<int>, int> Expert::review(const RunProject & project) {
     std::vector<Task *> tasks = project.consult_tasks();
     std::vector<int> remaning_tasks;
     
-    int remaning_time = 0;
+    int remaning_time = tasks.front()->durationParallelized();
 
-    throw new NotImplemented();
-    std::cout << "test" << std::endl;
+    for (int i = tasks.size() - 1; i >= 0; i--) {
+        Task *task = tasks[i];
+        if (!task->isAccomplished())
+            remaning_tasks.push_back(task->getId());
+    }
+
     return {remaning_tasks, remaning_time};
 }
